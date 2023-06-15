ad5815d4e91e19174c0d9d8d37996fb6361c3042
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-06-15 23:09:40 +0700

    Revert "Revert "added new navbar and carousel""
    This reverts commit 3ba2f8cebbb4617286b11257b8606f447a014a9a.

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,8 +2,51 @@
 
 @section('container')
 
-<h1 class="homepage">
-    ini adalah test home
-</h1>
+<div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="/carousel/company-meeting.jpg" class="image-carousel" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="/carousel/desktop.jpg" class="image-carousel" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="/carousel/working.jpg" class="image-carousel" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<div class="about-us">
+    <h1>Ini adalah page about us</h1>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum rerum dolores, aperiam et cupiditate magni facere. Placeat, optio voluptates ullam harum repellat error quisquam animi quaerat neque perferendis ab! Amet recusandae sint reiciendis, nisi facere qui mollitia consequuntur accusamus nostrum quidem aut optio magnam fugit! Amet possimus expedita soluta nihil! Libero sit assumenda non. In nam voluptatibus quasi provident alias ad dolore quidem, sapiente unde dolorum consequatur atque quibusdam cupiditate magni voluptates natus odit possimus? Eum, deleniti pariatur? Dolor, corporis. Magnam aut unde culpa praesentium aliquam, natus recusandae in a ut! Consectetur magni voluptate laborum illo minima rerum veritatis id ipsa, iure quidem omnis cupiditate asperiores praesentium, earum iste, optio doloribus. Ex impedit ipsum corrupti eos dolores? Illo autem illum vero rerum officiis debitis sit nisi eos quae alias qui similique ea inventore, repellat quibusdam ab vel, obcaecati veniam, molestiae repellendus error tenetur incidunt fuga totam? Quas commodi sequi laudantium ipsum nobis ipsam cum vitae asperiores harum maiores accusantium temporibus vero facilis incidunt neque fugiat voluptatum quibusdam eius rerum, similique in repellendus sint consectetur optio. Consectetur dignissimos dicta autem nisi unde ratione, optio similique saepe obcaecati in illum soluta velit architecto qui ullam. Dolor saepe tenetur fugiat neque dolorem, laborum dolore natus ut itaque est quo aspernatur? Perferendis, accusamus cumque quidem at voluptatem maiores excepturi, commodi impedit ea accusantium vero debitis consequuntur corrupti veniam velit nam omnis! Expedita facere minus enim distinctio sunt, dicta possimus. Laboriosam minima doloremque non, molestiae quis enim fugit veniam, incidunt magnam ab similique. Deserunt dolor eveniet voluptate odio ut sapiente recusandae doloribus nisi minus temporibus voluptatibus laborum deleniti fugiat ipsam assumenda eos excepturi sit, distinctio, ex ipsa. Nobis beatae ullam autem odio quos ex mollitia. Quae minus dolorem tenetur quis! Dolorem, nobis consequuntur atque quisquam vitae vero, velit quam a nihil tempora laborum beatae laboriosam quibusdam aperiam. Alias voluptatem ipsam quae est architecto cupiditate neque sed, vero deserunt magni libero consectetur ab dignissimos quam quo atque! Doloribus inventore vel soluta possimus vitae odit dolorem nulla recusandae accusamus eveniet at ad consectetur, aut quam? Perferendis, sequi. Recusandae, amet eum, fugit nobis iure fugiat esse error sequi cumque, accusamus sit officiis ut debitis tenetur cum aut vero culpa nam voluptatem eveniet. Iste aut maxime reprehenderit libero iure cum. Tempore quasi in mollitia ut ipsum, vitae quibusdam, maiores necessitatibus doloribus assumenda voluptatem quas porro, officiis consequatur placeat? Placeat ab dolore ut nulla architecto illo recusandae. Dignissimos explicabo velit id vitae officiis provident harum enim modi ab architecto at, laborum a, temporibus veniam accusantium saepe! Sapiente natus optio exercitationem quo. Sequi, quibusdam quos porro ex deleniti blanditiis facere fugiat deserunt recusandae dolorem earum odit soluta commodi eum voluptatum ipsa in! Animi sunt esse dolore, voluptatem asperiores blanditiis fugiat ullam exercitationem vero similique. Aut nulla numquam quos voluptatibus, aliquam aliquid praesentium animi atque, voluptatem minus impedit, nam corporis culpa! Praesentium assumenda tempore, maiores accusamus deleniti recusandae adipisci delectus sit sequi corrupti maxime distinctio, amet consequatur quos nostrum asperiores voluptates. Nam consequuntur soluta, fugit doloribus iure labore ipsum suscipit delectus facilis vitae provident perferendis, repellat, saepe tempore esse reprehenderit voluptatum minima laborum quas nesciunt similique ad. Alias aliquid fuga fugiat doloremque reprehenderit hic eos quasi nam cum. Omnis nostrum architecto, aut illum exercitationem sapiente consequuntur accusamus itaque doloremque deserunt, quidem consequatur illo neque alias magnam modi voluptas. Commodi ad distinctio maxime at debitis quae eveniet quasi ab vitae sapiente voluptatibus maiores esse doloribus soluta dolorem quaerat ipsa incidunt vero animi, sed nobis adipisci doloremque. Recusandae in aut vel fuga adipisci impedit quidem eum cumque qui, temporibus tempora ut? Deleniti nulla impedit assumenda a soluta consectetur hic! Minima, ipsam nulla beatae maiores velit commodi quae consectetur labore esse, molestiae ullam eius culpa amet reprehenderit assumenda at quasi ab praesentium recusandae repellendus vel magnam provident. Veniam id quibusdam debitis sapiente corrupti dolor, ratione laudantium laborum quas accusantium reprehenderit, voluptate aliquid nemo. Distinctio ipsum qui sequi molestiae temporibus soluta officia voluptate sit incidunt nisi ea harum modi id ipsa, possimus accusamus suscipit? Nihil similique itaque ab unde, fugiat praesentium, aut excepturi, earum ut magnam aliquam debitis ea. Voluptatibus tenetur iure quis unde quaerat natus assumenda velit repellat minus hic fuga nostrum quas cumque accusamus sed est vero obcaecati delectus praesentium error, eos itaque expedita totam? Nemo quisquam praesentium nostrum, alias ea delectus accusamus illum eaque harum laborum quidem labore ab non facilis ipsam vel, quo unde quaerat. Eos facilis beatae consectetur sed voluptate veritatis! Nisi quod accusantium sapiente eaque, quos soluta assumenda id! Repudiandae debitis nobis aspernatur nesciunt aut commodi, sapiente laboriosam non ratione error, recusandae sint! Ratione reiciendis blanditiis dolores veritatis ad voluptates! Facere fugit est autem magni sint maiores ad itaque deserunt. Distinctio, excepturi beatae impedit blanditiis ab ipsum repellat dolor voluptas mollitia deleniti a vero provident repudiandae nihil adipisci! Quos sint itaque, adipisci explicabo voluptatem repellat omnis, maiores aspernatur ut quod unde fugit rem quis alias. Ipsa error, architecto deserunt iure voluptatum voluptatibus. Reiciendis quod consequuntur molestias voluptatum odit vitae excepturi non vero sed reprehenderit blanditiis quas itaque laborum labore, quisquam dolor quibusdam corrupti minus? Dolorum temporibus ut minus ad dicta cupiditate cum accusamus, placeat sint quis repudiandae facilis? Cum doloribus aperiam fuga eius ullam soluta voluptates, quaerat alias deserunt distinctio, rerum quod reprehenderit recusandae molestias ut inventore repellendus minima quis atque? Modi assumenda obcaecati placeat, nemo fugit quibusdam eum labore nihil cum fuga est cupiditate ut possimus quisquam tempore architecto dolore, voluptatem consequatur sed aut sit voluptatum minus accusantium. Vitae eum dicta unde officiis voluptatem sit maiores, quidem fuga earum est, culpa doloribus corrupti aut suscipit commodi magni pariatur! Ullam assumenda dolore ipsam facere consequatur iusto velit animi expedita doloribus, illum nostrum cum. Eum beatae repellat numquam tenetur, fugiat quaerat soluta, odio eligendi ipsam adipisci reprehenderit. Consectetur hic earum accusamus nesciunt tenetur veritatis similique officiis vero iusto architecto molestias voluptate, aliquid nam molestiae ratione reiciendis incidunt voluptatum sint sed, aspernatur explicabo obcaecati assumenda! Numquam quaerat ipsam earum perferendis veritatis rem pariatur nostrum! Animi, exercitationem eius minima accusamus explicabo vero ratione laboriosam tempora neque aperiam ab illo tenetur voluptate harum earum? 
+    </p>
+
+</div>
 
 @endsection
