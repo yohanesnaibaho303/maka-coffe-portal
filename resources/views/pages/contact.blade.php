91393d1a10973aa395ec945303099b2755421121
Genaro Phenex Arya <77026486+GenaroSinaga@users.noreply.github.com>
2023-06-22 23:08:08 +0700

    add footer

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,68 +1,26 @@
-<div class="container-contact100" id="contact-section">
-  <div class="wrap-contact100">
-    <form class="contact100-form validate-form">
-      <span class="contact100-form-title"> Send Us A Message</span>
-      <label class="label-input100" for="first-name">Tell us your name *</label>
-      <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Type first name">
-        <input id="first-name" class="input100" type="text" name="first-name" placeholder="First name">
-        <span class="focus-input100"></span>
+<section class="contact" id="contact-section">
+  <h1 class="heading">contact</h1>
+  <div class="row">
+  <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.909440207648!2d107.07549089999999!3d-6.2756368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698f8b628aaa91%3A0xc5285227898ff6ff!2sMaka%20Coffee%20House!5e0!3m2!1sen!2sid!4v1687443662501!5m2!1sen!2sid" allowfullscreen="" loading="lazy"></iframe>
+    <form action="">
+      <h3>Send a Message</h3>
+      <div class="inputBox">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/>
+    </svg>
+        <input type="text" placeholder="name">
       </div>
-      <div class="input100 rs2-wrap-input100 validate-input" data-validate="Type last name">
-        <input class="input100" type="text" name="last-name" placeholder="Last name">
-        <span class="focus-input100"></span>
+      <div class="inputBox">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/>
+    </svg>
+        <input type="email" placeholder="email">
       </div>
-      <label class="label-input100" for="email">Enter your email</label>
-      <div class="wrap-input100 validate-input" data-validate="Valid email is required:ex@abc.xys">
-        <input id="email" class="input100" type="text" name="email" placeholder="Eg. example@gmail.com">
-        <span class="focus-input100"></span>
+      <div class="inputBox">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+    </svg>
+        <input type="number" placeholder="number">
       </div>
-      <label class="label-input100" for="phone">Enter phone number</label>
-      <div class="wrap-input100">
-        <input id="phone" class="input100" type="text" name="phone" placeholder="Eg. 08127638846">
-        <span class="focus-input100"></span>
-      </div>
-      <label class="label-input100" for="message">Message</label>
-      <div class="wrap-input100 validate-input" data-validate="Message is required">
-        <textarea id="message" class="input100" name="message" placeholder="Write us a message"></textarea>
-        <span class="focus-input100"></span>
-      </div>
-      <div class="container-contact100-form-btn">
-        <button class="contact100-form-btn"> Send Message </button>
-      </div>
+      <input type="submit" value="contact now" class="btn">
     </form>
-    <div class="contact100-more flex-col-c-m" style="background-image: url('/logo/8.png');">
-      <!-- ::before -->
-      <div class="flex-w sizel p-b-47">
-        <div class="txt1 p-r-25">
-          <span class="lnr lnr-map-marker">
-            <!-- :before -->
-          </span>
-        </div>
-        <div class="flex-col size2">
-          <span class="txt1 p-b-20"> Address </span>
-          <span class="txt2"> jalan haji nawi no 88 </span>
-        </div>
-      </div>
-      <div class="dis-flex size1 p-b-47">
-        <span class="lnr lnr-phone-handset">
-          <!-- :before -->
-        </span>
-      </div>
-      <div class="flex-col size2">
-        <span class="text1 p-b-20"> Lets Talk </span>
-        <span class="text3"> 081294757 </span>
-      </div>
-    </div>
-    <div class="dis-flex size1 p-b-47">
-      <div class="txt1 p-r-25">
-        <span class="lnr lnr-envelope">
-          <!-- ::before -->
-        </span>
-      </div>
-      <div class="flex-col size2">
-        <span class="txt1 p-b-20"></span>
-        <span class="txt3">contact@example.com</span>
-      </div>
-    </div>
   </div>
-</div>
+
+</section>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,51 @@
-<!-- <h1 class="bg-secondary">
-    ini adalah test footer
-</h1> -->
+<footer id="footer-bottom">
+    <div class="container">
+        <!-- ::before -->
+        <div class="footer-bottom">
+            <div class="footer-bottom-1 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div id="text-17" class="slidingbar-widget-col widget_text">
+                    <p>
+                        <strong>PT MAKA COFFEE</strong>
+                    </p>
+                    <p>
+                        "Jalan haji nawi jakarta selatan"
+                        <br>
+                        "cikarang utara lemah abang"
+                    </p>
+                </div>
+                <div style="clear:both;"></div>
+            </div>
+            <div id="cutom_html-6"class="widget_text slidingbar-widget-col widget_custom_html">
+                <!-- Site Footer -->
+                <div class="edgtf-footer-bottom-holder">
+                    <div class="edgtf-footer-bottom-holder-inner">
+                        <div class="edgtf-container">
+                            <!-- ::before -->
+                            <div class="edgtf-three-columns-inner">
+                                <div class="edgtf-column">
+                                    <div class="edgtf-column-inner">
+                                        <div id="text-13" class="widget edgtf-footer-bottom-left widget_text">
+                                            <div class="textwidget">
+                                            <p>&copy; 2023 Genaro Sinaga&nbsp;Maka Coffee - all rights reserved</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="edgtf-column">
+                                    <div class="edgtf-column-inner">
+                                        <a class="edgtf-social-icon-widget-holder" href="https://www.instagram.com/makacoffeehouse/" target="_bl">
+                                            <span class="edgtf-social-icon-widget-widget icomoon-icon-instagram">
+                                                <!-- ::before -->
+                                            </span>
+                                        </a>
+                                        <a class="edgtf-social-icon-widget-holder" href="https://goo.gl/maps/w1ydNpjieR3s4bEd8" target="_blank">
+                                            <span class="edgtf-social-icon-widget icomon-icon-google">
+                                                <!-- ::before -->
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
