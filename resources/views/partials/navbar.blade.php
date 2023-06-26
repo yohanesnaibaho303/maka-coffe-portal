061df2b10756ba9fc0b0d73558a0b4691a155f82
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-06-26 11:21:03 +0700

    fixed navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,6 @@
-<header>
-    <a href="" class="logo"><img src="/logo/logo-maka3.png" alt=""></a>
+<div class="navbar-01">
+    <a href="/" class="logo"><img src="/logo/logo-maka-new.png" alt=""></a>
     <div class="bx bx-menu" id="menu-icon"></div>
-
     <ul class="navbar">
         <li><a href="#shop-section">Home</a></li>
         <li><a href="#about-section">About</a></li>
@@ -9,4 +8,4 @@
         <li><a href="#menu-section">Menu</a></li>
         <li><a href="#contact-section">Contact</a></li>
     </ul>
-</header>
+</div>
